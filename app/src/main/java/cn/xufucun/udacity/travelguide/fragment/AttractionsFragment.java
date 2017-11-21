0a01d49f137ec9a797f111c9886844c1a18a7d34
xufucun <xufucun@gmail.com>
2017-11-21 20:51:06 +0800

    1

---
+++ app/src/main/java/cn/xufucun/udacity/travelguide/fragment/AttractionsFragment.java
@@ -0,0 +1,9 @@
+package cn.xufucun.udacity.travelguide.fragment;
+
+/**
+ * 景点Fragment
+ * Created by xufuc on 2017/11/21.
+ */
+
+public class AttractionsFragment {
+}
---
+++ app/src/main/java/cn/xufucun/udacity/travelguide/fragment/CateFragment.java
@@ -0,0 +1,8 @@
+package cn.xufucun.udacity.travelguide.fragment;
+
+/**
+ * Created by xufuc on 2017/11/21.
+ */
+
+public class CateFragment {
+}
